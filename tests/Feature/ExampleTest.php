3460d088d8a01d9c77eccc55969dd0213c740497
Arushad Ahmed <dash-8x@hotmail.com>
2024-01-30 16:07:19 +0500

    - Initial commit

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Javaabu\{YourPackage}\Tests\Feature;
+
+use Javaabu\{YourPackage}\Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     */
+    public function test_the_feature_is_true(): void
+    {
+        $this->assertTrue(true);
+    }
+}
