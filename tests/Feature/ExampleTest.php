1f79dd28da8713a8496ba07f951a20dc0646f5bb
WovenCoast <wovencoast14782@techie.com>
2025-03-06 09:42:29 +0500

    Fix PHPUnit Deprecations in tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,10 +3,11 @@
 namespace Javaabu\{YourPackage}\Tests\Feature;
 
 use Javaabu\{YourPackage}\Tests\TestCase;
+use PHPUnit\Framework\Attributes\Test;
 
 class ExampleTest extends TestCase
 {
-    /** @test */
+    #[Test]
     public function it_can_test_the_feature_is_true(): void
     {
         $this->assertTrue(true);
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -3,10 +3,11 @@
 namespace Javaabu\{YourPackage}\Tests\Unit;
 
 use Javaabu\{YourPackage}\Tests\TestCase;
+use PHPUnit\Framework\Attributes\Test;
 
 class ExampleTest extends TestCase
 {
-    /** @test */
+    #[Test]
     public function it_can_test_it_is_true(): void
     {
         $this->assertTrue(true);
