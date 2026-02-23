5b766fccde74eb5f097643c06721c354a86fdaa1
Arushad Ahmed <dash-8x@hotmail.com>
2026-02-23 15:50:24 +0500

    - Added Laravel 13 support - Dropped support for Laravel 9 and 10 - Dropped support for PHP 8.1

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,11 +2,12 @@
 
 namespace Javaabu\{YourPackage}\Tests\Feature;
 
+use PHPUnit\Framework\Attributes\Test;
 use Javaabu\{YourPackage}\Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /** @test */
+    #[Test]
     public function it_can_test_the_feature_is_true(): void
     {
         $this->assertTrue(true);
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,11 +2,12 @@
 
 namespace Javaabu\{YourPackage}\Tests\Unit;
 
+use PHPUnit\Framework\Attributes\Test;
 use Javaabu\{YourPackage}\Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /** @test */
+    #[Test]
     public function it_can_test_it_is_true(): void
     {
         $this->assertTrue(true);
